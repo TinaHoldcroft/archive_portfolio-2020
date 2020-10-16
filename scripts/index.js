03a36c559688d01e5ecb9bb3ceed61c3524eaecd
TinaHoldcroft <54176460+TinaHoldcroft@users.noreply.github.com>
2020-10-16 21:32:14 +0200

    overveiw img imp

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,1 @@
-console.log ('Tina Mary Holdcroft \n Business Intelligence Developer \n Front-end Developer \n Webdesigner \n Oslo, Norway \n \u00A9 TinaMary.com')
+console.log ('Tina Mary Holdcroft \n Front-end Developer \n Webdesigner \n Oslo, Norway \n \u00A9 TinaMary.com')
